c079c8f5175836419c1a0144613bf613d3975499
YonYoon <zhzhzh1806@gmail.com>
2024-04-24 14:48:22 +0500

    make quote changing on click

--- slilrespu/app.py
+++ slilrespu/app.py
@@ -1,13 +1,14 @@
+import json
 import random
 import sqlite3
 
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
+    # conn.row_factory = sqlite3.Row
     return conn
 
 # Function to initialize the database if it doesn't exist
@@ -22,13 +23,16 @@ init_db()
 
 @app.route("/")
 def index():
-    quote = get_random_quote()
-    return render_template('index.html', quote=quote)
+    json_quote = get_random_quote()
+    quote = json.loads(json_quote.data)
+    return render_template('index.html', quote=quote[0])
 
 
+@app.route("/get")
 def get_random_quote():
     conn = get_db_connection()
     quotes = conn.execute('SELECT * FROM quotes').fetchall()
     conn.close()
     quote = random.choice(quotes)
-    return quote[0]
+    json_quote = jsonify(quote)
+    return json_quote
