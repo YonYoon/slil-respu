3e010e4b77587b8b6a01fef86dbb15e0ea56a4af
YonYoon <zhzhzh1806@gmail.com>
2024-04-25 17:32:57 +0500

    remove commented line of code

--- slilrespu/app.py
+++ slilrespu/app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
-    # conn.row_factory = sqlite3.Row
     return conn
 
 # Function to initialize the database if it doesn't exist
