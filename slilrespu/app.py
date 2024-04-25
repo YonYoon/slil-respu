2c19f5bc75359abe91a9b9fc6c8d6aed0a50260d
YonYoon <zhzhzh1806@gmail.com>
2024-04-25 21:21:32 +0500

    implement unique quote and image on every click

--- slilrespu/app.py
+++ slilrespu/app.py
@@ -7,6 +7,9 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+current_quote = None
+current_image = None
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     return conn
@@ -30,20 +33,31 @@ def index():
 
 @app.route("/get")
 def get_random_quote():
+    global current_quote
+
     conn = get_db_connection()
-    quotes = conn.execute('SELECT * FROM quotes').fetchall()
+    if current_quote is None:
+        quotes = conn.execute('SELECT * FROM quotes').fetchall()
+    else:
+        quotes = conn.execute('SELECT * FROM quotes WHERE id != (?)', (current_quote,)).fetchall()
     conn.close()
     quote = random.choice(quotes)
     json_quote = jsonify(quote[1].upper())
+    current_quote = quote[0]
     return json_quote
 
 
 @app.route("/image")
 def get_random_image():
+    global current_image
+    
     current_directory = os.path.dirname(__file__)
     relative_path = 'static/images/'
     directory = os.path.join(current_directory, relative_path)
     image_names = os.listdir(directory)
     image_name = random.choice(image_names)
+    while image_name == current_image:
+        image_name = random.choice(image_names)
     json_name = jsonify(image_name)
+    current_image = image_name
     return json_name
